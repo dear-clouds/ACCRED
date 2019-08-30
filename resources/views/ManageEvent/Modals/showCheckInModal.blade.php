74ef83d8bb58c48ee3594c95e4339933c376fe7e
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-08-30 13:26:21 +0200

    Add return false jquery

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -1,3 +1,14 @@
+<html>
+<head>
+
+
+{!! HTML::script('vendor/jquery/dist/jquery.min.js') !!}
+
+
+</head>
+<body>
+
+
 
 <div role="dialog"  class="modal fade" style="display: none;">
    {!! Form::model($attendee, array('url' => route('postCheckInEditAttendee', array('event_id' => $event->id, 'attendee_id' => $attendee->id)), 'class' => 'ajax')) !!}
@@ -92,56 +103,6 @@
 
 <h2>Check-in</h2>
 
-<script>
-$(function () {
-  var wrapper = document.getElementById("signature-pad"),
-      clearButton = wrapper.querySelector("[data-action=clear]"),
-      saveButton = wrapper.querySelector("[data-action=save]"),
-      canvas = wrapper.querySelector("canvas"),
-      signaturePad;
-
-  // Adjust canvas coordinate space taking into account pixel ratio,
-  // to make it look crisp on mobile devices.
-  // This also causes canvas to be cleared.
-  window.resizeCanvas = function () {
-    var ratio =  window.devicePixelRatio || 1;
-    canvas.width = canvas.offsetWidth * ratio;
-    canvas.height = canvas.offsetHeight * ratio;
-    canvas.getContext("2d").scale(ratio, ratio);
-  }
-
-  resizeCanvas();
-
-  signaturePad = new SignaturePad(canvas);
-
-  clearButton.addEventListener("click", function(event) {
-    signaturePad.clear();
-  });
-
-  saveButton.addEventListener("click", function(event) {
-    event.preventDefault();
-
-    if (signaturePad.isEmpty()) {
-      alert("Please provide a signature first.");
-    } else {
-      var dataUrl = signaturePad.toDataURL();
-      var image_data = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
-
-      $.ajax({
-        url: '/save-signature',
-        type: 'POST',
-        data: {
-          image_data: image_data,
-        },
-      }).done(function() {
-        //
-      });
-    }
-  });
-});
-
-</script>
-
                               <div id="signature-pad" class="m-signature-pad">
                                 <div class="m-signature-pad--body">
                                   <canvas style="border: 2px dashed #ccc"></canvas>
@@ -154,6 +115,57 @@ $(function () {
                               </div>
 
 
+
+                              <script>
+                              $(function () {
+                                var wrapper = document.getElementById("signature-pad"),
+                                    clearButton = wrapper.querySelector("[data-action=clear]"),
+                                    saveButton = wrapper.querySelector("[data-action=save]"),
+                                    canvas = wrapper.querySelector("canvas"),
+                                    signaturePad;
+
+                                // Adjust canvas coordinate space taking into account pixel ratio,
+                                // to make it look crisp on mobile devices.
+                                // This also causes canvas to be cleared.
+                                window.resizeCanvas = function () {
+                                  var ratio =  window.devicePixelRatio || 1;
+                                  canvas.width = canvas.offsetWidth * ratio;
+                                  canvas.height = canvas.offsetHeight * ratio;
+                                  canvas.getContext("2d").scale(ratio, ratio);
+                                }
+
+                                resizeCanvas();
+
+                                signaturePad = new SignaturePad(canvas);
+
+                                clearButton.addEventListener("click", function(event) {
+                                  signaturePad.clear();
+                                });
+
+                                saveButton.addEventListener("click", function(event) {
+                                  event.preventDefault();
+
+                                  if (signaturePad.isEmpty()) {
+                                    alert("Please provide a signature first.");
+                                  } else {
+                                    var dataUrl = signaturePad.toDataURL();
+                                    var image_data = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+
+                                    $.ajax({
+                                      url: '/save-signature',
+                                      type: 'POST',
+                                      data: {
+                                        image_data: image_data,
+                                      },
+                                    }).done(function() {
+                                      //
+                                    });
+                                  }
+                                });
+                                return false;
+                              });
+
+                              </script>
 
                               <h2>Enveloppe</h2>
                               {{$attendee->enveloppe}}
@@ -185,6 +197,7 @@ $(function () {
 </div>
 
 
+@include("Shared.Partials.LangScript")
 {!! HTML::script('assets/javascript/backend.js') !!}
 <script>
     $(function () {
@@ -203,3 +216,5 @@ $(function () {
 
 </script>
 <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+</body>
+</html>
