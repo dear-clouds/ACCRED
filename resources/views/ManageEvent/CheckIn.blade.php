d0dc1e5467d551d5ac9607de6f49410931a4febe
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-08-30 13:15:48 +0200

    Test Signature Pad

--- resources/views/ManageEvent/CheckIn.blade.php
+++ resources/views/ManageEvent/CheckIn.blade.php
@@ -103,56 +103,6 @@
 
                                 <button data-modal-id="showCheckInModal@{{ attendee.id }}" href="javascript:void(0);"  data-href="/event/{{ $event_id }}/check_in/@{{ attendee.id }}/modal" class="loadModal btn btn-success" type="button">Check-in</button>
 
-                                <script>
-                                $(function () {
-                                  var wrapper = document.getElementById("signature-pad"),
-                                      clearButton = wrapper.querySelector("[data-action=clear]"),
-                                      saveButton = wrapper.querySelector("[data-action=save]"),
-                                      canvas = wrapper.querySelector("canvas"),
-                                      signaturePad;
-
-                                  // Adjust canvas coordinate space taking into account pixel ratio,
-                                  // to make it look crisp on mobile devices.
-                                  // This also causes canvas to be cleared.
-                                  window.resizeCanvas = function () {
-                                    var ratio =  window.devicePixelRatio || 1;
-                                    canvas.width = canvas.offsetWidth * ratio;
-                                    canvas.height = canvas.offsetHeight * ratio;
-                                    canvas.getContext("2d").scale(ratio, ratio);
-                                  }
-
-                                  resizeCanvas();
-
-                                  signaturePad = new SignaturePad(canvas);
-
-                                  clearButton.addEventListener("click", function(event) {
-                                    signaturePad.clear();
-                                  });
-
-                                  saveButton.addEventListener("click", function(event) {
-                                    event.preventDefault();
-
-                                    if (signaturePad.isEmpty()) {
-                                      alert("Please provide a signature first.");
-                                    } else {
-                                      var dataUrl = signaturePad.toDataURL();
-                                      var image_data = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
-
-                                      $.ajax({
-                                        url: '/save-signature',
-                                        type: 'POST',
-                                        data: {
-                                          image_data: image_data,
-                                        },
-                                      }).done(function() {
-                                        //
-                                      });
-                                    }
-                                  });
-                                });
-
-                                </script>
-
                         <span class="ci btn btn-successfulQrRead">
                             <i class="ico-checkmark"></i>
                         </span>
