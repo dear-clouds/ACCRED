a981f5e70b74ffec69c95a94a4bf0a1c495656d0
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-08-30 13:30:32 +0200

    test SignaturePad

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -92,6 +92,58 @@
 
 <h2>Check-in</h2>
 
+    <script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+
+    <script>
+    $(function () {
+      var wrapper = document.getElementById("signature-pad"),
+          clearButton = wrapper.querySelector("[data-action=clear]"),
+          saveButton = wrapper.querySelector("[data-action=save]"),
+          canvas = wrapper.querySelector("canvas"),
+          signaturePad;
+
+      // Adjust canvas coordinate space taking into account pixel ratio,
+      // to make it look crisp on mobile devices.
+      // This also causes canvas to be cleared.
+      window.resizeCanvas = function () {
+        var ratio =  window.devicePixelRatio || 1;
+        canvas.width = canvas.offsetWidth * ratio;
+        canvas.height = canvas.offsetHeight * ratio;
+        canvas.getContext("2d").scale(ratio, ratio);
+      }
+
+      resizeCanvas();
+
+      signaturePad = new SignaturePad(canvas);
+
+      clearButton.addEventListener("click", function(event) {
+        signaturePad.clear();
+      });
+
+      saveButton.addEventListener("click", function(event) {
+        event.preventDefault();
+
+        if (signaturePad.isEmpty()) {
+          alert("Please provide a signature first.");
+        } else {
+          var dataUrl = signaturePad.toDataURL();
+          var image_data = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
+
+          $.ajax({
+            url: '/save-signature',
+            type: 'POST',
+            data: {
+              image_data: image_data,
+            },
+          }).done(function() {
+            //
+          });
+        }
+      });
+    });
+
+    </script>
+
                               <div id="signature-pad" class="m-signature-pad">
                                 <div class="m-signature-pad--body">
                                   <canvas style="border: 2px dashed #ccc"></canvas>
@@ -105,55 +157,7 @@
 
 
 
-                              <script>
-                              $(function () {
-                                var wrapper = document.getElementById("signature-pad"),
-                                    clearButton = wrapper.querySelector("[data-action=clear]"),
-                                    saveButton = wrapper.querySelector("[data-action=save]"),
-                                    canvas = wrapper.querySelector("canvas"),
-                                    signaturePad;
 
-                                // Adjust canvas coordinate space taking into account pixel ratio,
-                                // to make it look crisp on mobile devices.
-                                // This also causes canvas to be cleared.
-                                window.resizeCanvas = function () {
-                                  var ratio =  window.devicePixelRatio || 1;
-                                  canvas.width = canvas.offsetWidth * ratio;
-                                  canvas.height = canvas.offsetHeight * ratio;
-                                  canvas.getContext("2d").scale(ratio, ratio);
-                                }
-
-                                resizeCanvas();
-
-                                signaturePad = new SignaturePad(canvas);
-
-                                clearButton.addEventListener("click", function(event) {
-                                  signaturePad.clear();
-                                });
-
-                                saveButton.addEventListener("click", function(event) {
-                                  event.preventDefault();
-
-                                  if (signaturePad.isEmpty()) {
-                                    alert("Please provide a signature first.");
-                                  } else {
-                                    var dataUrl = signaturePad.toDataURL();
-                                    var image_data = dataUrl.replace(/^data:image\/(png|jpg);base64,/, "");
-
-                                    $.ajax({
-                                      url: '/save-signature',
-                                      type: 'POST',
-                                      data: {
-                                        image_data: image_data,
-                                      },
-                                    }).done(function() {
-                                      //
-                                    });
-                                  }
-                                });
-                              });
-
-                              </script>
 
                               <h2>Enveloppe</h2>
                               {{$attendee->enveloppe}}
@@ -183,6 +187,3 @@
        {!! Form::close() !!}
     </div>
 </div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
