1b0e166ad30852aaa53adb1d06483ab60a89de1e
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-09-02 12:20:26 +0200

    SignaturePad test #5

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -1,5 +1,4 @@
-{!! HTML::script('vendor/jquery/dist/jquery.min.js') !!}
-<script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
+
 
 <div role="dialog"  class="modal fade" style="display: none;">
    {!! Form::model($attendee, array('url' => route('postEditAttendee', array('event_id' => $event->id, 'attendee_id' => $attendee->id)), 'class' => 'ajax')) !!}
@@ -199,3 +198,6 @@
        {!! Form::close() !!}
     </div>
 </div>
+
+{!! HTML::script('vendor/jquery/dist/jquery.min.js') !!}
+<script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
