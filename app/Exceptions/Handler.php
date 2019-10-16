3b907402bd6dc7fce8e9c81350a3f90ca3df1b02
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-10-16 10:57:59 +0200

    Handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,16 +44,6 @@ class Handler extends ExceptionHandler
      }
  }
 
- public function report(Exception $exception)
-{
-    if (app()->bound('sentry') && $this->shouldReport($exception)){
-        app('sentry')->captureException($exception);
-    }
-
-    parent::report($exception);
-}
-
-
 
     /**
      * Render an exception into an HTTP response.
