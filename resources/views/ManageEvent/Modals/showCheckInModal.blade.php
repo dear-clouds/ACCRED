273289b8a15825f240d55f2820c288d38cb938ed
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-11-06 16:52:06 +0100

    Debug: Check-in button

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -193,7 +193,7 @@
             </div> <!-- /end modal body-->
             <div class="modal-footer">
 
-              <form method="post" action="{{route('postCheckInAttendee', ['event_id' => $event->id])}}">
+              <form method="post" action="{{route('postCheckInAttendee', ['event_id' => $event->id, 'attendee_id' => $attendee->id, 'checking' => $attendee->has_arrived])}}">
               <button type="submit" name="check-in" class="btn btn-success">Check-in</button>
             </form>
 
