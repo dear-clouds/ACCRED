be5fec5ecfef3cf1a2e48ef69076bcbc4e3b55cf
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-12-03 11:31:27 +0100

    Test signature

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -718,7 +718,7 @@ Route::group(
                 'uses' => 'EventCheckInController@postCheckInEditAttendee',
             ]);
 
-            Route::post('{event_id}/check_in/{attendee_id}/signature', [
+            Route::post('/event/signature', [
                 'as'   => 'saveSignature',
                 'uses' => 'EventCheckInController@saveSignature',
             ]);
