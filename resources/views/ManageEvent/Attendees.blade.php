839ac4894e8ed6b7487813a019b1b9bbcf017880
Manorie KAM-OON <m.kam-oon@fiawec.com>
2019-12-05 12:26:58 +0100

    Test sort attendees

--- resources/views/ManageEvent/Attendees.blade.php
+++ resources/views/ManageEvent/Attendees.blade.php
@@ -80,7 +80,7 @@
                     <thead>
                         <tr>
                             <th>
-                               {!!Html::sortable_link(trans("Attendee.name"), $sort_by, 'last_name', $sort_order, ['q' => $q , 'page' => $attendees->currentPage()])!!}
+                               {!!Html::sortable_link(trans("Attendee.name"), $sort_by, 'last_name', $sort_order, ['q' => $q , 'page' => $attendees->total()])!!}
                             </th>
                             <th>
                                {!!Html::sortable_link(trans("Attendee.company"), $sort_by, 'company', $sort_order, ['q' => $q , 'page' => $attendees->currentPage()])!!}
