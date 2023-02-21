c561ffc6ca6fc557dcc02ecf0ca0d8adcc05d557
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:27:21 +0700

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -27,6 +34,7 @@ return [
     */
 
     'default' => env('DB_TYPE', 'mysql'),
+    'default' => env('DB_CONNECTION', 'heroku'),
 
     /*
     |--------------------------------------------------------------------------
@@ -91,6 +99,17 @@ return [
             'password' => env('DB_PASSWORD', ''),
             'prefix'   => '',
         ],
+        
+        'heroku' => array(
+    'driver' => 'mysql',
+    'host' => $host,
+    'database' => $database,
+    'username' => $username,
+    'password' => $password,
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => '',
+),
 
     ],
 
