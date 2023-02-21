fe7ac2fd40b1401f742941f33bc3dd9f10b0f742
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:20:53 +0700

    Co-authored-by: Mio.

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -135,6 +135,18 @@
                                 //
                                 // resizeCanvas();
 
+                                function resizeCanvas() {
+  var ratio =  Math.max(window.devicePixelRatio || 1, 1);
+  canvas.width = canvas.offsetWidth * ratio;
+  canvas.height = canvas.offsetHeight * ratio;
+  canvas.getContext("2d").scale(ratio, ratio);
+  signaturePad.clear(); // otherwise isEmpty() might return incorrect value
+}
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+
                                 signaturePad = new SignaturePad(canvas);
 
                                 clearButton.addEventListener("click", function(event) {
