4c32034c185a7aee0f610ec3fa0e8fbc293dc5f9
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:29:19 +0700

    change default files

--- config/database.php
+++ config/database.php
@@ -1,12 +1,5 @@
 <?php
 
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -34,7 +27,7 @@ return [
     */
 
     'default' => env('DB_TYPE', 'mysql'),
-    'default' => env('DB_CONNECTION', 'heroku'),
+    'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -102,10 +95,10 @@ return [
         
         'heroku' => array(
     'driver' => 'mysql',
-    'host' => $host,
-    'database' => $database,
-    'username' => $username,
-    'password' => $password,
+    'host' => '',
+    'database' => '',
+    'username' => '',
+    'password' => '',
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
