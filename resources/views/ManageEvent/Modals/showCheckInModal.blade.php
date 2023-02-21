95e90379d06127081b36c1f7f2a371b175fdaa74
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:20:14 +0700

    Co-authored-by: Mio.

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -198,6 +198,3 @@
        {!! Form::close() !!}
     </div>
 </div>
-
-{!! HTML::script('vendor/jquery/dist/jquery.min.js') !!}
-<script src="https://cdn.jsdelivr.net/npm/signature_pad@2.3.2/dist/signature_pad.min.js"></script>
