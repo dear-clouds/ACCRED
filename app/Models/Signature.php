a30975697e734719a4866b9e62a2f3f56e778538
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:20:22 +0700

    Co-authored-by: Mio.

---
+++ app/Models/Signature.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use File;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PDF;
+
+class Signature extends MyBaseModel
+{
+    use SoftDeletes;
+    /**
+     * The attendees associated with the order.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function attendees()
+    {
+        return $this->hasMany(\App\Models\Attendee::class);
+    }
+
+    /**
+     * The account associated with the order.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function account()
+    {
+        return $this->belongsTo(\App\Models\Account::class);
+    }
+
+    /**
+     * The event associated with the order.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function event()
+    {
+        return $this->belongsTo(\App\Models\Event::class);
+    }
+
+    /**
+     * The tickets associated with the order.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tickets()
+    {
+        return $this->hasMany(\App\Models\Ticket::class);
+    }
+
+
+
+    /**
+     * Get the full name of the order.
+     *
+     * @return string
+     */
+    public function getFullNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+}
