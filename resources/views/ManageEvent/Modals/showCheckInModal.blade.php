ab9178358e2080cde500412f700e0d1f8a8de231
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:20:35 +0700

    Co-authored-by: Mio.

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -97,7 +97,7 @@
                         <div class="row">
                             <div class="col-md-12">
 
-<form method="post" action="{{ action('EventCheckInController@postCheckInAttendee') }}" accept-charset="UTF-8">
+
 
                               <h2>Check-in</h2>
 
@@ -192,14 +192,10 @@
                 </div>
             </div> <!-- /end modal body-->
             <div class="modal-footer">
-
-
-              <button type="submit"><i class="glyphicon glyphicon-remove"></i>Check-in</button>
-
-
+              <button @click="toggleCheckin(attendee)"></i>Check-in</button>
 
             </div>
         </div><!-- /end modal content-->
-</form>
+
     </div>
 </div>
