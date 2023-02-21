597bbf4e44b665aebf3ba31b9706ff2dfca53fe5
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:20:44 +0700

    Co-authored-by: Mio.

--- app/Http/Controllers/EventCheckInController.php
+++ app/Http/Controllers/EventCheckInController.php
@@ -228,7 +228,6 @@ class EventCheckInController extends MyBaseController
             'checked' => $checking,
             'message' =>  (($checking == 'in') ? trans("Controllers.attendee_successfully_checked_in") : trans("Controllers.attendee_successfully_checked_out")),
             'id'      => $attendee->id,
-            'redirectUrl' => '/event/' . $event_id . '/check_in',
         ]);
     }
 
