99e5328ce3f350fe485bc2ce442c9f3f534b924b
Mio <mio.bluewatermelon@gmail.com>
2023-02-21 11:20:47 +0700

    Co-authored-by: Mio.

--- resources/views/ManageEvent/Modals/showCheckInModal.blade.php
+++ resources/views/ManageEvent/Modals/showCheckInModal.blade.php
@@ -207,3 +207,12 @@
 
     </div>
 </div>
+<script>
+    $(function () {
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+            }
+        });
+    });
+</script>
